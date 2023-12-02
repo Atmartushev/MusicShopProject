97704d38d602e8f3771b1bca9fe1c6e3de9ec764
JDNET\RGGU97M <martushevavtonoma@johndeere.com>
2023-12-02 12:03:17 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MusicShopProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
